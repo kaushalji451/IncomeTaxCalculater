c73206c5bba64f70154e66a42ed8107c6bd55126
abhishek kaushal <abhishekkaushal2526@gmail.com>
2025-04-05 17:37:44 +0530

    first commit

---
+++ backend/README.md
@@ -0,0 +1,8 @@
+# Incometax calculator api 
+this is a microservice to calculate the income tax by getting the user details like ,
+gross income,year of bussiness, nature of bussiness.
+
+How to Setup----
+1. clone the reposetry
+2. Install dependencies:
+npm install
---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port  = 8080;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/tax', (req, res) => {
+  res.send('Tax information');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
